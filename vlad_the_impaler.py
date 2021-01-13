a880c4db9fdc99a83b41f17e29698f3fad16275d
ldscu <ludescu@outlook.com>
2021-01-13 04:12:18 +0000

    0b175a1f24070eae7cd07d0b6738d20c

--- vlad_the_impaler.py
+++ vlad_the_impaler.py
@@ -75,7 +75,7 @@ async def on_message(message):
 async def on_raw_message_delete(payload):
     channel = client.get_channel(798663330516172830)
     message = payload.cached_message
-    await channel.send(f"[{datetime.datetime.now()}] '{message.author}' a șters mesajul ăsta în '#{message.channel}': '{message.content}'")
+    await channel.send(f"[{datetime.datetime.now()}] {message.author} a șters mesajul ăsta în #{message.channel} [{message.content}]")
 
 @client.event
 async def on_member_join(member):
